f72a7798b84a613e69388b695bbdc29e72b9cbe3
Viorel Spinu <viorel.spinu@gmail.com>
2020-06-08 11:55:45 +0300

    adaugat scriptul de search

---
+++ povesti.js
@@ -0,0 +1,15 @@
+function search() {
+
+    let storyToSearch = document.getElementById('searchbox').value;
+
+    let stories = document.getElementsByClassName("story");
+
+    for (let i = 0; i < stories.length; i++) {
+        let storyText = stories[i].innerHTML.toLowerCase();
+        if (storyText.includes(storyToSearch)) {
+            stories[i].style.display = "block";
+        } else {
+            stories[i].style.display = "none";
+        }
+    }
+}
